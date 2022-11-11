304cd599975363779fb221c70ce9c27718285039
VenkataRevanthNaidu <92348355+VenkataRevanthNaidu@users.noreply.github.com>
2022-11-11 02:16:46 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Page is owned by vdanala{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
